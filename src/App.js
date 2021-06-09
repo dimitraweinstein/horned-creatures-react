dc18384d3a1fc48bca66246b8079f2e7a945767c
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-08 20:14:53 -0700

    finished keyword filter method

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ export default class App extends Component {
 
   state = {
     filteredHorns: '',
+    filteredKeyword: '',
   };
 
   handleHornChange = (e) => {
     this.setState({ filteredHorns: e.target.value })
+  };
+
+  handleKeywordChange = (e) => {
+    this.setState({ filteredKeyword: e.target.value })
   };
 
   render() {
@@ -21,13 +26,21 @@ export default class App extends Component {
     console.log(filteredHorns)
     if(this.state.filteredHorns) {
       filteredHorns = filteredHorns.filter( creature => Number(this.state.filteredHorns) === creature.horns)
-    }
+      console.log(filteredHorns)
+    } 
+
+    let filteredKeyword = data;
+    console.log(filteredKeyword)
+    if(this.state.filteredKeyword ) {
+      filteredKeyword = filteredKeyword.filter( creature => this.state.filteredKeyword === creature.keyword)
+      console.log(filteredHorns)
+    } 
 
 
   return (
     <div>
       <Header />
-      <ImageList images={data} />
+      <ImageList data={ filteredHorns, filteredKeyword } />
       <form>
         <label>
           <select onChange={this.handleHornChange}>
@@ -36,6 +49,24 @@ export default class App extends Component {
             <option value='2'>2</option>
             <option value='3'>3</option>
             <option value='100'>100</option>
+          </select>
+      </label>
+    </form>
+    <form>
+        <label>
+          <select onChange={this.handleKeywordChange}>
+            <option value=''>All</option>
+            <option value='narwhal'>Narwhal</option>
+            <option value='rhino'>Rhino</option>
+            <option value='unicorn'>Unicorn</option>
+            <option value='unilego'>UniLego</option>
+            <option value='triceratops'>Triceratops</option>
+            <option value='markhor'>Markhor</option>
+            <option value='mouflon'>Mouflon</option>
+            <option value='addax'>Addax</option>
+            <option value='chameleon'>Chameleon</option>
+            <option value='lizard'>Lizard</option>
+            <option value='dragon'>Dragon</option>
           </select>
       </label>
     </form>
--- src/ImageList.js
+++ src/ImageList.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import images from './data.js';
 import ImageItem from './ImageItem.js';
 
 export default class ImageList extends Component {
     render() {
         return (
             <div>
-                {images.map((image, i) => 
+                {this.props.data.map((image, i) => 
                 <ImageItem url={image.url} key={i} />
                 )
                 }
